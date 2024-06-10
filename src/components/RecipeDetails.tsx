4edb5b22718f476a12e32c1cfe236561bf0f46fa
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-06-10 19:53:07 +0200

    restyle the recipedetails component.

--- src/components/RecipeDetails.tsx
+++ src/components/RecipeDetails.tsx
@@ -9,6 +9,9 @@ import {
   Text,
   UnorderedList,
   Button,
+  Box,
+  flexbox,
+  Flex,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -26,7 +29,9 @@ const RecipeDetails = () => {
 
   useEffect(() => {
     apiClient
-      .get("recipes/" + id, {headers: {"x-auth-token" : localStorage.getItem("authToken")}})
+      .get("recipes/" + id, {
+        headers: { "x-auth-token": localStorage.getItem("authToken") },
+      })
       .then((res) => {
         setRecipe(res.data);
         setRating(getReviewAverage(res.data));
@@ -34,18 +39,24 @@ const RecipeDetails = () => {
       .catch((err) => console.log(err));
   }, []);
 
-  function getReviewAverage (recipe: Recipe) {
-    return recipe.reviews.length == 0 ? 0 : recipe.reviews.map((a: any) => a.rating).reduce((a: any, b: any) => a + b, 0) / recipe.reviews.length
+  function getReviewAverage(recipe: Recipe) {
+    return recipe.reviews.length == 0
+      ? 0
+      : recipe.reviews
+          .map((a: any) => a.rating)
+          .reduce((a: any, b: any) => a + b, 0) / recipe.reviews.length;
   }
 
   function handleDelete() {
     if (!recipe) return;
 
     apiClient
-      .delete(`/recipes/${id}`, {headers: {"x-auth-token" : localStorage.getItem("authToken")}})
+      .delete(`/recipes/${id}`, {
+        headers: { "x-auth-token": localStorage.getItem("authToken") },
+      })
       .then((response) => {
         console.log("Recipe Deleted", response.data);
-        navigate('/');
+        navigate("/");
       })
       .catch((error) => {
         console.log("Something Went Wrong", error);
@@ -55,54 +66,64 @@ const RecipeDetails = () => {
   const saveRating = (ratingValue: number) => {
     setRating(ratingValue);
     apiClient
-        .put(`recipes/${id}/rating`, { rating: ratingValue }, {headers: {"x-auth-token" : localStorage.getItem("authToken")}})
-        .catch(console.error);
+      .put(
+        `recipes/${id}/rating`,
+        { rating: ratingValue },
+        { headers: { "x-auth-token": localStorage.getItem("authToken") } }
+      )
+      .catch(console.error);
   };
 
   return (
     <div>
-      <Card borderRadius="10px" overflow="hidden" width="80%">
-        <Image src={recipe?.image} height="50%" width="100%" />
-        <CardBody>
-          <Heading mb={3} fontSize="2xl">
-            {recipe?.title}
-          </Heading>
-          <Text>Category: {recipe?.category}</Text>
-          <Text mb={4}>{recipe?.description}</Text>
-          <UnorderedList my={5}>
-            <Heading as="h2">Ingredients</Heading>
-            {recipe?.ingridients.map((ing, i) => (
-              <ListItem key={i}>{ing}</ListItem>
-            ))}
-          </UnorderedList>
-          <OrderedList>
-            <Heading as="h2">Instructions</Heading>
-            {recipe?.instructions.map((instruction, i) => (
-              <ListItem key={i}>{instruction}</ListItem>
-            ))}
-          </OrderedList>
-        </CardBody>
-        <CardFooter>
-          <Text>Author: {recipe?.author}</Text>
-        </CardFooter>
-        <StarRating rating={rating} setRating={saveRating}/>
-      </Card>
-      <Button
-        colorScheme="red"
-        size="sm"
-        onClick={handleDelete}
-        mr="2"
-      >
-        Delete
-      </Button>
-
-      <Button
-        colorScheme="yellow"
-        size="sm"
-        mr="2"
-      >
-        <Link to={"/recipes/" + id + "/edit"}>Edit recipe</Link>
-      </Button>
+      <Box>
+        <Card borderRadius="10px" overflow="hidden">
+          <Flex gap="4">
+          <Box>
+              <CardBody>
+                <Heading mb={3} fontSize="3xl">
+                  {recipe?.title}
+                </Heading>
+                <Text>Category: {recipe?.category}</Text>
+                <Text mb={4}>{recipe?.description}</Text>
+                <UnorderedList>
+                  <Heading as="h2" my={2} >Ingredients</Heading>
+                  {recipe?.ingridients.map((ing, i) => (
+                    <ListItem key={i}>{ing}</ListItem>
+                  ))}
+                </UnorderedList>
+                <OrderedList>
+                  <Heading as="h2" my={2}>Instructions</Heading>
+                  {recipe?.instructions.map((instruction, i) => (
+                    <ListItem key={i}>{instruction}</ListItem>
+                  ))}
+                </OrderedList>
+              </CardBody>
+              <CardFooter>
+                <Text>Author: {recipe?.author}</Text>
+              </CardFooter>
+              <StarRating rating={rating} setRating={saveRating} />
+              <Flex mt={4}>
+                <Button colorScheme="yellow" size="sm" m="2">
+                  <Link to={"/recipes/" + id + "/edit"}>Edit recipe</Link>
+                </Button>
+                <Button
+                  colorScheme="red"
+                  size="sm"
+                  onClick={handleDelete}
+                  m="2"
+                >
+                  Delete
+                </Button>
+              </Flex>
+            </Box>
+            <Box w="700px">
+              <Image borderRadius={20} src={recipe?.image} />
+            </Box>
+            
+          </Flex>
+        </Card>
+      </Box>
     </div>
   );
 };
